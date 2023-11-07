d4ff8552f8780b1def02c874f125c3e0a3164122
029 <anbschool0029@029s-Mac-mini.local>
2023-11-07 14:41:01 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./utils/components/Home";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
